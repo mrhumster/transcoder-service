edf0db71cd3e704bae6318da28c365df9f4d76ec
Om Ra <me@xomrkob.ru>
2026-04-16 10:57:54 +0600

    ping redis

--- internal/worker/asynq.go
+++ internal/worker/asynq.go
@@ -1,10 +1,14 @@
 package worker
 
 import (
+	"context"
+	"log/slog"
+	"os"
 	"time"
 
 	"github.com/hibiken/asynq"
 	"github.com/mrhumster/transcoder-service/config"
+	"github.com/redis/go-redis/v9"
 )
 
 func NewAsynqWorker(c *config.Config) *asynq.Server {
@@ -13,6 +17,14 @@ func NewAsynqWorker(c *config.Config) *asynq.Server {
 		Password: c.Redis.Passwrod,
 		DB:       c.Redis.DB,
 	}
+
+	checkClient := redisClientOpt.MakeRedisClient().(redis.UniversalClient)
+	if err := checkClient.Ping(context.Background()).Err(); err != nil {
+		slog.Error("Redis connection failed", "error", err)
+		os.Exit(1)
+	}
+	checkClient.Close()
+
 	asynqConfig := asynq.Config{
 		Concurrency:     c.Worker.Concurrency,
 		ShutdownTimeout: c.Worker.ShutdownTimeout * time.Minute,
