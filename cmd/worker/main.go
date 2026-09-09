9e3be091d46c7bfa3e4f994c2f678b2feeb77233
Om Ra <me@xomrkob.ru>
2026-09-09 12:50:53 +0600

    feat: version from git via ldflags instead of hardcoded

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -17,8 +17,13 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+var (
+	version   = "dev"
+	buildDate = "unknown"
+)
+
 func main() {
-	wellcome := figure.NewFigure("transcoder v0.1.0", "graffiti", true)
+	wellcome := figure.NewFigure("transcoder "+version, "graffiti", true)
 	wellcome.Print()
 	opts := &slog.HandlerOptions{
 		Level:     slog.LevelDebug,
